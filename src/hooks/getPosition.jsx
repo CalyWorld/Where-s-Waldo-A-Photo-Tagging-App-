7c14817812a018ae1151869d219b236df25ff7b2
Cal <obinnaanizoba5@gmail.com>
2023-02-27 22:16:19 +0800

    object to stoore images in dropdown menu with respective coordinates and id

--- src/hooks/getPosition.jsx
+++ src/hooks/getPosition.jsx
@@ -1,22 +1,49 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { Waldo } from "../components/waldo";
+import { v4 as uuidv4 } from "uuid";
+import waldoP from "/Users/cal/Where-s-Waldo-A-Photo-Tagging-App-/src/assets/waldoP.png";
+import odlaw from "/Users/cal/Where-s-Waldo-A-Photo-Tagging-App-/src/assets/odlaw.jpg";
+import wizard from "/Users/cal/Where-s-Waldo-A-Photo-Tagging-App-/src/assets/wizard.jpeg";
 
-export const GetPosition = () =>{
+export const GetPosition = () => {
+  const [mousePosition, setPosition] = useState({});
 
-    const [mousePosition, setPosition] = useState({});
+  const [photoTagInfo, setPhotoTagging] = useState([
+    {
+      src: waldoP,
+      x: "",
+      y: "",
+      id: uuidv4(),
+    },
+    {
+      src: odlaw,
+      x: "",
+      y: "",
+      id: uuidv4(),
+    },
+    {
+      src: wizard,
+      x: "",
+      y: "",
+      id: uuidv4(),
+    },
+  ]);
 
-    const getPosition = (e, setModal) =>{
-        const getX = e.clientX - e.target.offsetLeft;
-        const getY = e.clientY - e.target.offsetTop;
+  const getPosition = (e, setModal) => {
+    const getX = e.clientX - e.target.offsetLeft;
+    const getY = e.clientY - e.target.offsetTop;
 
-        
-        setPosition({x:getX + 30, y:getY + 50});
-        setModal(true);
-    }
+    console.log(getX, getY);
 
-    return(
-        <>
-        <Waldo mousePosition={mousePosition} getPosition={getPosition}/>
-        </>
-    )
-}
+    setPosition({ x: getX + 30, y: getY + 50 });
+    setModal(true);
+  };
+
+  console.log(photoTagInfo.map((eachPhoto)=>eachPhoto.src))
+
+  return (
+    <>
+      <Waldo mousePosition={mousePosition} photoTagInfo={photoTagInfo} getPosition={getPosition} />
+    </>
+  );
+};
