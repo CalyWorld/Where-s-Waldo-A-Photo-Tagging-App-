7c4dd5bb94ba86d7a0a6b8d7678ce18fd6279491
Cal <obinnaanizoba5@gmail.com>
2023-02-27 20:50:46 +0800

    changed from using classnames to style to build position property of where dropdown modal drops when image is clicked dynamically

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -3,17 +3,27 @@ import waldoP from "/Users/cal/Where-s-Waldo-A-Photo-Tagging-App-/src/assets/wal
 import odlaw from "/Users/cal/Where-s-Waldo-A-Photo-Tagging-App-/src/assets/odlaw.jpg";
 import wizard from "/Users/cal/Where-s-Waldo-A-Photo-Tagging-App-/src/assets/wizard.jpeg";
 export const Modal = ({ setModal, mousePosition }) => {
-  // console.log(mousePosition.getPositionX, mousePosition.getPositionY);
+  console.log(mousePosition.y, mousePosition.x);
 
   return (
     <div
-      className={`absolute top-${mousePosition.getPositionY} left-${mousePosition.getPositionX}`}
+      style={{
+        position: "absolute",
+        left: `${mousePosition.x}px`,
+        top: `${mousePosition.y}px`,
+      }}
     >
       <ul>
         {" "}
         <li className="flex flex-col w-40 justify-start bg-slate-50 items-start">
           <span className="flex flex-row w-40 justify-end">
-            <button onClick={()=>{setModal(false)}}>X</button>
+            <button
+              onClick={() => {
+                setModal(false);
+              }}
+            >
+              X
+            </button>
           </span>
           <img className="h-10 w-10" src={waldoP} alt="left" />
           <img className="h-10 w-10" src={odlaw} alt="middle" />
